f8b4a22bc9bc73676d9a644b913710bce217dee7
RandomFractals <RandomFractals@users.noreply.github.com>
2023-05-24 17:08:29 -0500

    document settings.ts (#5)

--- src/commands/settings.ts
+++ src/commands/settings.ts
@@ -4,8 +4,14 @@ import {
   Uri
 } from 'vscode';
 
+/**
+ * Evidence app setting file location to configure data sources.
+ */
 const settingsFilePath = '.evidence/template/evidence.settings.json';
 
+/**
+ * Opens Evidence settings file for editing.
+ */
 export function openSettingsFile() {
   if (!workspace.workspaceFolders) {
     window.showErrorMessage('This command is only available when you have an Evidence project workspace open.');
