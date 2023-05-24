70e74b502919dc3efc1d6e844576fd20e72a1fe2
RandomFractals <RandomFractals@users.noreply.github.com>
2023-05-24 17:02:33 -0500

    check for node_modules prior to running build commands (#8)

--- src/commands/build.ts
+++ src/commands/build.ts
@@ -1,4 +1,19 @@
+import {
+  workspace,
+  FileStat,
+  FileType,
+  Uri
+} from 'vscode';
+
 import { sendCommand } from '../terminal';
+
+/**
+ * Node modules folder name to check in the open project workspace
+ * for installed Evidence app NodeJS dependencies.
+ *
+ * @see https://docs.evidence.dev/getting-started/install-evidence
+ */
+const nodeModules = `node_modules`;
 
 /**
  * Installs Evidence app NodeJS dependencies.
@@ -15,7 +30,7 @@ export function installDependencies() {
  * @see https://docs.evidence.dev/deployment/overview#build-process
  */
 export function buildProject() {
-  sendCommand('npm run build');
+  executeCommand('npm run build');
 }
 
 /**
@@ -24,5 +39,40 @@ export function buildProject() {
  * @see https://docs.evidence.dev/deployment/overview#buildstrict
  */
 export function buildProjectStrict() {
-  sendCommand('npm run build:strict');
+  executeCommand('npm run build:strict');
+}
+
+/**
+ * Checks node modules dependencies,
+ * install them if they don't exist,
+ * and sends the requested project build command
+ * to the Evidence terminal instance.
+ *
+ * @param command Terminal command to execute.
+ */
+export async function executeCommand(command: string) {
+  if (!(await hasDependencies())) {
+    installDependencies();
+  }
+  sendCommand(command);
+}
+
+/**
+ * Checks if open Evidence project has /node_modules folder
+ * and NodeJS dependencies installed.
+ *
+ * @see https://docs.evidence.dev/getting-started/install-evidence
+ */
+export async function hasDependencies(): Promise<boolean> {
+  if (workspace.workspaceFolders) {
+    const nodeModulesUri: Uri = Uri.joinPath(workspace.workspaceFolders[0].uri, nodeModules);
+    try {
+      const nodeModulesStat: FileStat = await workspace.fs.stat(nodeModulesUri);
+      return nodeModulesStat.type === FileType.Directory;
+    }
+    catch (error) {
+      return false;
+    }
+  }
+  return false;
 }
