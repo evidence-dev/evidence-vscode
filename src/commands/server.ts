0b13bbd68b6a186a11efb1e86dee5a8cedd0b8f5
RandomFractals <RandomFractals@users.noreply.github.com>
2023-05-24 17:13:31 -0500

    install nodejs dependencies prior to starting app dev server (#4)

--- src/commands/server.ts
+++ src/commands/server.ts
@@ -1,10 +1,18 @@
+import { executeCommand } from './build';
 import { sendCommand } from '../terminal';
 
+/**
+ * Starts Evidence app dev server.
+ */
 export function startServer() {
-  sendCommand('npm run dev');
+  executeCommand('npm run dev');
 }
 
+/**
+ * Stops running app dev server.
+ */
 export function stopServer() {
+  // send ctrl+c to stop running dev server
   sendCommand('\x03', '', false); // shift focus to terminal
   //sendCommand('q', '', false);
 }
