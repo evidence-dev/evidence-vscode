8791ed8e1f365424182418a979f9c49f8152acde
RandomFractals <RandomFractals@users.noreply.github.com>
2023-05-24 15:11:14 -0500

    refine no workspace folder error message in settings.ts (#5)

--- src/commands/settings.ts
+++ src/commands/settings.ts
@@ -8,7 +8,7 @@ const settingsFilePath = '.evidence/template/evidence.settings.json';
 
 export function openSettingsFile() {
   if (!workspace.workspaceFolders) {
-    window.showErrorMessage('This command is only available when you have a workspace open.');
+    window.showErrorMessage('This command is only available when you have an Evidence project workspace open.');
   }
   else {
     const settingsPath = Uri.joinPath(workspace.workspaceFolders[0].uri, settingsFilePath);
