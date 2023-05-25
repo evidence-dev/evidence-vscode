a0ddfe131683e7aabdbcd001ea6164bae4f0d7c6
RandomFractals <RandomFractals@users.noreply.github.com>
2023-05-25 09:37:55 -0500

    add get npm version and is supported node v. to node.ts (#15)

--- src/node.ts
+++ src/node.ts
@@ -10,6 +10,45 @@ export async function getNodeVersion() {
 }
 
 /**
+ * Checks if the provided NodeJS version string
+ * meets the major and minor version requirements.
+ *
+ * @param nodeVersion NodeJS version string to check.
+ * @param majorVersion Major version number.
+ * @param minorVersion Minor version number.
+ *
+ * @returns Trus if NodeJS version is equal or greater
+ *  than the major and minor version numbers, and false otherwise.
+ */
+export function isSupportedNodeVersion(nodeVersion: string,
+  majorVersion: number, minorVersion: number): boolean {
+  // check node version
+  if (nodeVersion.startsWith('v')) {
+    const nodeVersionNumbers = nodeVersion.replace('v', '').split('.');
+    const majorVersionNumber = parseInt(nodeVersionNumbers[0]);
+    const minorVersionNumber = parseInt(nodeVersionNumbers[1]);
+
+    if (majorVersionNumber > majorVersion) {
+      return true;
+    }
+    else if (majorVersionNumber === majorVersion &&
+      minorVersionNumber >= minorVersion) {
+      return true;
+    }
+  }
+  return false;
+}
+
+/**
+ * Gets NPM version.
+ *
+ * @returns The NPM version.
+ */
+export async function getNpmVersion() {
+  return await executeCommand('npm --version');
+}
+
+/**
  * Executes command using node child_process.exec.
  *
  * @see https://nodejs.org/api/child_process.html#child_processexeccommand-options-callback
