f8cee0785270c341ff7871fb4e49a8f37ab16d49
RandomFractals <RandomFractals@users.noreply.github.com>
2023-05-27 06:03:22 -0500

    enhance preview to load pages from optional http, https and file scheme page Uris (#3)
    and add a link to the bilt-in simpler browser extension implementation in docs

--- src/commands/preview.ts
+++ src/commands/preview.ts
@@ -16,15 +16,29 @@ export const localAppUrl = 'http://localhost:3000';
  * in the built-in VSCode Simple Browser webview.
  *
  * @param uri Optional Uri of the page to preview.
+ *
+ * @see Simple browser extension implementation:
+ *  https://github.com/microsoft/vscode/pull/109276
  */
 export function preview(uri?: Uri) {
+  // default page url
   let pageUrl: string = localAppUrl;
-  if (uri && workspace.workspaceFolders) {
+
+  // create web page url from page Uri
+  if (uri && (uri.scheme === 'http' || uri.scheme === 'https')) {
+    pageUrl = uri.toString(true); // skip encoding
+  }
+  else if (uri && uri.scheme === 'file' && workspace.workspaceFolders) {
+    // get project folder root path
     const workspaceFolderPath: string = workspace.workspaceFolders[0].uri.fsPath;
+
+    // create web page url from file Uri by converting .md path to app page path
     let pagePath: string = uri.fsPath.replace(workspaceFolderPath, '')
       .split('\\').join('/').replace('/pages/', '')
       .replace('index.md', '').replace('.md', '');
     pageUrl = `${localAppUrl}/${pagePath}`;
   }
+
+  // open web page in the built-in simple browser webview
   commands.executeCommand(Commands.ShowSimpleBrowser, pageUrl);
 }
