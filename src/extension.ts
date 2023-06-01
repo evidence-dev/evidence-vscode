4c520585c221cf87a40be10ddc3e8808908a4a02
RandomFractals <RandomFractals@users.noreply.github.com>
2023-06-01 13:58:11 -0500

    implement Evidence dev server auto start and prompt to install dependencies (#53)
    on extension activation

--- src/extension.ts
+++ src/extension.ts
@@ -6,11 +6,12 @@ import {
 } from 'vscode';
 
 import { MarkdownSymbolProvider } from './providers/markdownSymbolProvider';
-
 import { setExtensionContext } from './extensionContext';
 import { registerCommands } from './commands/commands';
 import { loadPackageJson, hasDependency } from './utils/jsonUtils';
-import { updateProjectContext } from './config';
+import { Settings, getConfig, updateProjectContext } from './config';
+import { installDependencies } from './commands/build';
+import { startServer } from './commands/server';
 import { statusBar } from './statusBar';
 import { closeTerminal } from './terminal';
 
@@ -33,17 +34,33 @@ export async function activate(context: ExtensionContext) {
   const evidenceFiles = await workspace.findFiles('**/.evidence/**/*.*');
   if (workspace.workspaceFolders && evidenceFiles.length > 0 &&
     hasDependency(packageJson, '@evidence-dev/evidence')) {
+
+    // set Evidence project context
     updateProjectContext();
 
-    // check for node modules
+    // check for node modules and auto start dev server
     const nodeModules = await workspace.findFiles('**/node_modules/**/*.*');
+    const autoStart: boolean = <boolean>getConfig(Settings.AutoStart);
     if (nodeModules.length > 0) {
       // show start dev server status
       statusBar.showStart();
+
+      if (autoStart) {
+        startServer();
+      }
     }
     else {
       // show install node modules status
       statusBar.showInstall();
+
+      // prompt a user to install Evidence node.js dependencies
+      window.showInformationMessage(
+        'Would you like to install Evidence project node.js dependencies?', 'Yes', 'No')
+        .then((selection) => {
+          if (selection === 'Yes') {
+            installDependencies();
+          }
+        });
     }
   }
 }
