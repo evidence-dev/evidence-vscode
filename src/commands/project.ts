11e8cf01b2e88c0c97c190a76f4e6a16829e4e2e
RandomFractals <RandomFractals@users.noreply.github.com>
2023-06-03 09:10:46 -0500

    new `project.ts` with create new project dialog and folders list display (#48)

---
+++ src/commands/project.ts
@@ -0,0 +1,86 @@
+import {
+  window,
+  workspace,
+  Uri,
+  OutputChannel,
+  ProgressLocation
+} from 'vscode';
+
+import * as path from 'path';
+
+import { getOutputChannel } from '../output';
+
+/**
+ * Create a new Evidence project in the given folder.
+ *
+ * @param {Uri} projectFolderUri Optional project folder Uri to create the project in.
+ */
+export async function createNewProject(projectFolderUri?: Uri) {
+
+  if (!projectFolderUri) {
+    const selectedFolders: Uri[] | undefined = await showSelectFolderDialog();
+    if (!selectedFolders) {
+      // user cancelled folder selection and new Evidence project creation action
+      return;
+    }
+    else {
+      // get the first selected folder
+      projectFolderUri = selectedFolders[0];
+    }
+  }
+
+  // check for existing project files in the selected new project folder
+  const projectFiles = await workspace.findFiles(
+    Uri.joinPath(projectFolderUri, '**/*.*').fsPath);
+
+  if (projectFiles.length > 0 ) {
+    // prompt to select an empty new project folder
+    // and display the project selection dialog again
+    window.showInformationMessage(
+     'Select an empty folder to create a new Evidence project.');
+    createNewProject();
+    return;
+  }
+
+  // get new project folder path and name
+  const projectFolderPath = projectFolderUri.fsPath;
+  const projectFolderName = path.basename(projectFolderPath);
+
+  // display creating new Evidence project status in the output channel
+  const outputChannel: OutputChannel = getOutputChannel();
+  outputChannel.show();
+  outputChannel.append(`Creating new Evidence project in ${projectFolderPath} ...`);
+
+  window.withProgress({
+    location: ProgressLocation.Notification,
+    title: 'New Evidence Project',
+    cancellable: false,
+  }, async (progress, token) => {
+
+    let increment = 0;
+    progress.report({
+      increment: increment,
+      message: `Creating new Evidence project in ${projectFolderPath}`
+    });
+
+    progress.report({
+      increment: 100,
+      message: 'TODO: creat project from template in'+ projectFolderName });
+  });
+}
+
+/**
+ * Displays a dialog to select a folder.
+ *
+ * @returns The selected folder Uri, or undefined.
+ */
+export async function showSelectFolderDialog(): Promise<Uri[] | undefined> {
+  // show open dialog to select an empty folder for a new Evidence project
+  return await window.showOpenDialog({
+    title: 'New Evidence Project Folder',
+    canSelectFiles: false,
+    canSelectFolders: true,
+    canSelectMany: false,
+    openLabel: 'Select an empty folder to create new Evidence project.'
+  });
+}
