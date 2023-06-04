119e14a90bb8f70a6936232624de13afbd714ee9
RandomFractals <RandomFractals@users.noreply.github.com>
2023-06-04 06:08:32 -0500

    move showOpenFolder message display to new `prompts.ts` (#59)

--- src/views/prompts.ts
+++ src/views/prompts.ts
@@ -2,6 +2,9 @@ import {
   window
 } from 'vscode';
 
+import { commands, Uri } from 'vscode';
+
+import { Commands } from '../commands/commands';
 import { installDependencies } from '../commands/build';
 
 export async function showInstallDependencies() {
@@ -13,5 +16,27 @@ export async function showInstallDependencies() {
         installDependencies();
       }
     });
+
+}
+
+/**
+ * Displays Open Folder notification message,
+ * and opens it in a new VS Code window
+ * when a user confirms the Open Folder dialog selection.
+ *
+ * @param projectFolder Project folder to open.
+ */
+export async function showOpenFolder(projectFolder: Uri) {
+  // display Open Folder notification message
+  window.showInformationMessage(
+    `Evidence project created in: ${projectFolder.fsPath}.`,
+    'Open Folder'
+  ).then((selection: string | undefined) => {
+    if (selection === 'Open Folder') {
+      // open created project folder in a new VS Code window
+      // if the user selected the Open Folder option
+      commands.executeCommand(Commands.OpenFolder, projectFolder, true);
+    }
+  });
 
 }
