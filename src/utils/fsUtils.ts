dbe5e1edc88d434c710dcb408509498cae7a7107
RandomFractals <RandomFractals@users.noreply.github.com>
2023-06-06 05:48:16 -0500

    add new copyFiles to fsUtils.ts and enhance folder copy for the open workspace folder (#72)

--- src/utils/fsUtils.ts
+++ src/utils/fsUtils.ts
@@ -2,10 +2,13 @@ import {
   workspace,
   FileType,
   OutputChannel,
-  Uri
+  WorkspaceFolder,
+  Uri,
 } from 'vscode';
 
+import { getWorkspaceFolder } from '../config';
 import { getOutputChannel } from '../output';
+
 /**
  * Checks if the given folder exists using workspace.fs API.
  *
@@ -25,7 +28,7 @@ export async function folderExists(folder: Uri): Promise<boolean> {
 }
 
 /**
- * Copies template folder to the destination project folder.
+ * Copies template folder to the destination folder.
  *
  * @param templateFolder Template folder Uri.
  * @param destinationFolder Destination folder Uri.
@@ -38,15 +41,70 @@ export async function copyFolder(templateFolder: Uri, destinationFolder: Uri): P
   outputChannel.appendLine(`- Template Project: ${templateFolder.fsPath}\n`);
 
   try {
-    await workspace.fs.copy(templateFolder, destinationFolder, { overwrite: true });
-    outputChannel.appendLine(`✔ New project created successfully.`);
-    return true;
+    // get open workspace folder
+    const workspaceFolder: WorkspaceFolder | undefined = await getWorkspaceFolder();
+
+    if (workspaceFolder && workspaceFolder.uri.fsPath === destinationFolder.fsPath) {
+      // copy source folder files and subfolders into the open workspace folder
+      const filesCopied = await copyFiles(templateFolder, workspaceFolder.uri);
+      if (filesCopied) {
+        outputChannel.appendLine(`✔ New project created successfully in the open workspace folder: ${workspaceFolder.name}`);
+        return true;
+      }
+      return false; // folder copy into open workspace folder failed
+    }
+    else {
+      // copy template folder to the destination folder with overwrite option
+      await workspace.fs.copy(templateFolder, destinationFolder, { overwrite: true });
+      outputChannel.appendLine(`✔ New project created successfully.`);
+      return true;
+    }
   }
   catch (error) {
     outputChannel.appendLine('✗ Error copying template project:');
     outputChannel.appendLine(` ${error}`);
     return false;
   }
+}
+
+/**
+ * Creates new target folder, if it doesn't exist,
+ * and copies all the files and subfolders
+ * from the source folder to the target folder.
+ *
+ * @param sourceFolder The source folder Uri.
+ * @param targetFolder The target folder Uri.
+ */
+export async function copyFiles(sourceFolder: Uri, targetFolder: Uri) {
+
+  // get source folder files and subfolders
+  const sourceFiles = await workspace.fs.readDirectory(sourceFolder);
+
+  // create target folder if it doesn't exist
+  if (!await folderExists(targetFolder)) {
+    await workspace.fs.createDirectory(targetFolder);
+  }
+
+  // copy files and subfolders from the source to the target folder
+  for (const [name, type] of sourceFiles) {
+    // create source and file target Uris
+    const sourceFile: Uri = Uri.joinPath(sourceFolder, name);
+    const targetFile: Uri = Uri.joinPath(targetFolder, name);
+
+    if (type === FileType.File) {
+      // copy file
+      await workspace.fs.copy(sourceFile, targetFile);
+    }
+    else if (type === FileType.Directory) {
+      // create new target subfolder
+      await workspace.fs.createDirectory(targetFile);
+
+      // copy source subfolder to target folder
+      await workspace.fs.copy(sourceFile, targetFile, { overwrite: true });
+    }
+  }
+
+  return true;
 }
 
 /**
