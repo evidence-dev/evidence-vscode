e0bd250e92b2bd08f423578bec9179fc8127bb11
Archie Wood <58074498+archiewood@users.noreply.github.com>
2023-06-22 22:18:10 -0400

    query chaining

--- src/commands/query.ts
+++ src/commands/query.ts
@@ -1,4 +1,4 @@
-import { extensions, window, commands, ExtensionContext } from 'vscode';
+import { extensions, window, commands, ExtensionContext, workspace} from 'vscode';
 import { IExtension} from '@sqltools/types';
 import { syncSettings, readEvidenceSettingsFile } from '../settings';
 
@@ -58,7 +58,6 @@ export async function runQuery(name: string, query: string) {
   const sqltools = extensions.getExtension<IExtension>('mtxr.sqltools');
   // check if sqltools is installed
   if (sqltools) {
-    console.log('SQLTools is installed');
     if (!sqltools.isActive) {
       await sqltools.activate();
     }
@@ -67,21 +66,29 @@ export async function runQuery(name: string, query: string) {
     let matchingDB =false;
     supportedSqlToolsDrivers.forEach(async (db: any) => {
       if (db.id === settings.database) {
-        console.log(`Found matching SQLTools driver for ${db.name}`);
         //check if the driver is installed
         matchingDB = true;
         let driver = extensions.getExtension(db.extensionId);
         if (driver) {
-          console.log(`SQLTools ${db.name} driver is installed`);
           try {
             // sync settings from evidence to sqltools
             await syncSettings();
-            commands.executeCommand('sqltools.executeQuery', query);
+            // check if it is a chained query by looking for ${...} in the query with regex
+            let isChainedQuery = query.match(/\${.*}/); 
+            if (isChainedQuery) {
+              let compiledQuery = await getCompiledQuery(name);
+              try {
+                commands.executeCommand('sqltools.executeQuery', compiledQuery);
+              } catch (error) {
+                console.error(error);
+              }
+            } else {
+              commands.executeCommand('sqltools.executeQuery', query);
+            }
           } catch (error) {
             console.error('Error executing query with SQLTools:', error);
           }
         } else {
-          console.log(`SQLTools ${db.name} driver is not installed`);
           // Prompt the user to install the driver
           const choice = await window.showInformationMessage(`Install the SQLTools ${db.name} driver?`, 'Install', 'View In Marketplace');
           if (choice === 'Install') {
@@ -103,6 +110,30 @@ export async function runQuery(name: string, query: string) {
     }
     else if (choice === 'View In Marketplace') {
       commands.executeCommand('workbench.extensions.action.showExtensionsWithIds', ['mtxr.sqltools']);
+    }
+  }
+}
+
+/* Gets compiled query from evidennce file system. 
+/  This will fail if Evidence has not run since the query was created.
+/  And it will return old queries if the user has not saved the file containing the SQL, or if they are not on the right evidence page.
+/  Maybe this is too broken and we should disable chained queries for now.
+*/
+export async function getCompiledQuery(name: string) {
+  // read in compiled queries from .evidence/template/extracted/queries.json
+  // return the query with the matching name
+  let queryFiles = await workspace.findFiles('.evidence/template/.evidence-queries/extracted/**/queries.json');
+  if(queryFiles.length>0)
+  {
+    let queries = await workspace.fs.readFile(queryFiles[0]);
+    let queryList = JSON.parse(queries.toString());
+    let matchingQuery = queryList.find((q: any) => q.id === name);
+    // return compiled query
+    if (matchingQuery){
+      return matchingQuery.compiledQueryString;
+    } else {
+      window.showErrorMessage(`No compiled query found with name ${name}: Chained queries require Evidence to be running.`);
+      throw new Error(`No chanied query found with name ${name}: Chained queries require Evidence to be running.`);
     }
   }
 }
