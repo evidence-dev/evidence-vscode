2f80c005911be3e716c3b0a99adde3e41ed35716
Sean Hughes <sean@evidence.dev>
2023-12-14 11:58:25 -0500

    fix sources command for windows

--- src/commands/sources.ts
+++ src/commands/sources.ts
@@ -17,7 +17,7 @@ export async function runSources() {
     // check if we need to run command in a different directory than root of the project:
     const workspaceFolderPath = workspace.workspaceFolders ? workspace.workspaceFolders[0].uri.fsPath : '';
     const packageJsonFolder = await getPackageJsonFolder();
-    const cdCommand = packageJsonFolder ? `cd ${packageJsonFolder} && ` : '';
+    const cdCommand = packageJsonFolder ? `cd ${packageJsonFolder} ; ` : '';
     const cdBackCommand = packageJsonFolder ? `; cd ${workspaceFolderPath}` : '';
 
     sendCommand(`${cdCommand}npm run sources${cdBackCommand}`);
